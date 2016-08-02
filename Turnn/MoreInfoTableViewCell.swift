6a6424f0bf8a9e5e98a39691c5c83c05cef4f903
Steve Cox <coxsteve@gmail.com>
2016-08-02 15:29:38 -0600

    JUSTIN FILES!

---
+++ Turnn/MoreInfoTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  MoreInfoTableViewCell.swift
+//  PracticeCreateEvent
+//
+//  Created by Justin Smith on 8/2/16.
+//  Copyright © 2016 Justin Smith. All rights reserved.
+//
+
+import UIKit
+
+class MoreInfoTableViewCell: UITableViewCell, UITextViewDelegate {
+    
+    @IBOutlet weak var moreInfoLabel: UILabel!
+    @IBOutlet weak var moreInfoTextView: UITextView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        setupCell()
+    }
+    
+    func setupCell() {
+        self.backgroundColor = .clearColor()
+        self.moreInfoTextView.layer.cornerRadius = 8
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        if selected == true {
+            self.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.4)
+            
+            UIView.animateWithDuration(0.5) {
+                self.backgroundColor = .clearColor()
+            }
+        }
+
+    }
+
+}
---
+++ Turnn/ZipTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ZipTableViewCell.swift
+//  PracticeCreateEvent
+//
+//  Created by Justin Smith on 8/2/16.
+//  Copyright © 2016 Justin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ZipTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var zipTextField: UITextField!
+    @IBOutlet weak var spacerView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        setupCell()
+    }
+    
+    func setupCell() {
+        self.backgroundColor = .clearColor()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        if selected == true {
+            self.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.4)
+            
+            UIView.animateWithDuration(0.5) {
+                self.backgroundColor = .clearColor()
+            }
+        }
+
+    }
+
+}
