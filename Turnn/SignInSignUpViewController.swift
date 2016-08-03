4f5a91cc637fbb7f8e45f134cbfd8149e24d2e22
Steve Cox <coxsteve@gmail.com>
2016-08-03 12:13:56 -0600

    updated sign in/up VC

--- Turnn/SignInSignUpViewController.swift
+++ Turnn/SignInSignUpViewController.swift
@@ -9,27 +9,51 @@
 import UIKit
 
 class SignInSignUpViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViewUI()
     }
-
+    
+    func setupViewUI() {
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.278, green: 0.310, blue: 0.310, alpha: 1.00)
+        self.navigationController?.navigationBar.tintColor = UIColor(red: 0.000, green: 0.663, blue: 0.800, alpha: 1.00)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        setBackgroundForView()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+    func setBackgroundForView() {
+        
+        let blurEffect = UIBlurEffect(style: .Dark)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        
+        let imageView = UIImageView(image: UIImage(named: "Turnn Background")!)
+        imageView.contentMode = .Center
+        
+        imageView.addSubview(blurView)
+        self.view.insertSubview(imageView, atIndex: 0)
+        blurView.frame = imageView.frame
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
