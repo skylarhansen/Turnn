2be1aca9f5f825db974b9e4373563eab24fab0e6
Steve Cox <coxsteve@gmail.com>
2016-08-03 14:58:59 -0600

    adding event controller, but not project file

---
+++ Turnn/EventController.swift
@@ -0,0 +1,40 @@
+//
+//  EventController.swift
+//  Turnn
+//
+//  Created by Steve Cox on 8/3/16.
+//  Copyright © 2016 Skylar Hansen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventController {
+
+    static func createEvent(title: String, location: Location, startTime: NSDate, endTime: NSDate, categories: [Category], eventDescription: String? = "", passwordProtected: Bool? = false, password: String?, contactInfo: String?, image: UIImage?, moreInfo: String?)
+    {
+    let host = UserController.sharedController.currentUser
+        var event = Event(title: title, location: location, startTime: startTime, endTime: endTime, categories: categories, eventDescription: eventDescription, passwordProtected: passwordProtected, password: password, contactInfo: contactInfo, image: image, host: host, moreInfo: moreInfo)
+        event.save()
+    }
+    
+    static func fetchEvents(completion: (events: [Event]) -> Void){
+        FirebaseController.ref.child("Events").observeEventType(.Value, withBlock: { (dataSnapshot) in
+            guard let dataDictionary = dataSnapshot.value as? [String: [String: AnyObject]] else {
+                completion(events: [])
+                return
+            }
+            let events = dataDictionary.flatMap { Event(dictionary: $1, identifier: $0) }
+            completion(events: events)
+        })
+    }
+    
+    static func updateEvent(){
+    }
+    
+    static func deleteEvent(event: Event){
+        event.delete()
+    }
+    
+
+}
