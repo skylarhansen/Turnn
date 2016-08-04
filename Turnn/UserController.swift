572e41a26f7a996cbd8ac760d6878fe2488eaf4a
skylarhansen <skylarshansen@gmail.com>
2016-08-04 09:48:50 -0600

    updateUser func

--- Turnn/UserController.swift
+++ Turnn/UserController.swift
@@ -17,13 +17,11 @@ class UserController {
     private let kUser = "userKey"
     
     var currentUser: User? {
+        
         get {
             
             guard let uid = FIRAuth.auth()?.currentUser?.uid,
-                let userDictionary = NSUserDefaults.standardUserDefaults().valueForKey(kUser) as? [String: AnyObject] else {
-                    
-                    return nil
-            }
+                userDictionary = NSUserDefaults.standardUserDefaults().valueForKey(kUser) as? [String: AnyObject] else { return nil }
             
             return User(dictionary: userDictionary, identifier: uid)
         }
@@ -31,10 +29,13 @@ class UserController {
         set {
             
             if let newValue = newValue {
+                
                 NSUserDefaults.standardUserDefaults().setValue(newValue.dictionaryCopy, forKey: kUser)
                 print("User Login Values: \(newValue.dictionaryCopy) -- \(kUser)")
                 NSUserDefaults.standardUserDefaults().synchronize()
+                
             } else {
+                
                 NSUserDefaults.standardUserDefaults().removeObjectForKey(kUser)
                 NSUserDefaults.standardUserDefaults().synchronize()
             }
@@ -51,14 +52,14 @@ class UserController {
                 
             } else {
                 
-                
-                
                 print("User created.")
                 
                 FIRAuth.auth()?.signInWithEmail(email, password: password, completion: { (user, error) in
                     
                     if error != nil {
+                        
                         print("Error could not find user with email and password. --> \(error?.localizedDescription)")
+                        
                     } else {
                         
                         guard let user = user else { print("Did not get user info back for some reason... "); return }
@@ -87,6 +88,7 @@ class UserController {
     }
     
     func authUser(email: String, password: String) {
+        
         FIRAuth.auth()?.signInWithEmail(email, password: password, completion: { (user, error) in
             
             if error != nil {
@@ -101,11 +103,11 @@ class UserController {
                     })
                 }
             }
-            
         })
     }
     
     func deleteUser(user: User) {
+        
         user.delete()
         if let firebaseUser = FIRAuth.auth()?.currentUser {
             firebaseUser.deleteWithCompletion({ (error) in
@@ -116,4 +118,35 @@ class UserController {
         }
         currentUser = nil
     }
+    
+    func updateUser(firstName: String, lastName: String = "") {
+        
+        let user = FIRAuth.auth()?.currentUser
+        if let user = user {
+            let changeRequest = user.profileChangeRequest()
+            
+            changeRequest.displayName = "\(firstName) \(lastName)"
+            changeRequest.commitChangesWithCompletion({ (error) in
+                if let error = error {
+                    print("Unable to update user: \(user.uid) --> \(error.localizedDescription)")
+                } else {
+                    print("User info updated. UID = \(user.uid)")
+                }
+            })
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
